513a7d1c09f681f83ee070e9c5777b90f99e9764
warlyware <danward@gmail.com>
2017-04-02 08:53:10 -0400

    Add CORS to keystone

--- keystone.js
+++ keystone.js
@@ -30,6 +30,8 @@ keystone.init({
 // Load your project's Models
 keystone.import('models');
 
+keystone.set('cors allow origin', true);
+
 // Setup common locals for your templates. The following are required for the
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
